e5c9c345661e2804d0aeba596ac2044ad99d5430
amazbhola <amazingbhola@gmail.com>
2021-01-24 12:17:03 +0600

    class Modifi

--- index.php
+++ index.php
@@ -2,57 +2,6 @@
 <?php include 'partials/nav.php'; ?>
 
 <?php
-  /**
-   *
-   */
-  /**
-   *
-   */
-  class OOP
-  {
-
-    function __construct()
-    {
-    echo "wellcome to PHP world";
-    }
-    public function sayHello()
-    {
-      echo "Hello World";
-    }
-    public function __destruct()
-    {
-      echo "<br /> I am about to desappear - bye bye";
-    }
-  }
-  $obj = new OOP;
-  echo "<br />";
-  $obj->sayHello();
-
-
-
- ?>
-<hr />
-<?php
-  class oopdes{
-      private $name;
-    public function __construct($val)
-    {
-      return $this->name=$val;
-    }
-    public function sayHello2()
-    {
-      echo "Hello World";
-    }
-
-    public function __destruct()
-    {
-      echo "<br /> Bye Bye $this->name!";
-    }
-  }
-  $objects = new oopdes('Sohel');
-  echo "<br />";
-  $objects->sayHello2();
- ?>
 
 
 <?php include 'partials/footer.php'; ?>
